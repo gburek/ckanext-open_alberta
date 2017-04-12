68644a7bb25c1e529f70497fc6cb8c6ae83d9ac1
Greg Burek <grzegorz.burek@gov.ab.ca>
2017-04-12 15:18:54 -0600

    Checkbox styling, including a javascript patching.

--- ckanext/open_alberta/fanstatic/js/fixes.js
+++ ckanext/open_alberta/fanstatic/js/fixes.js
@@ -1,4 +1,9 @@
+/* Various fixes */
+
 $(function() {
+    /* Styling checkboxes requires an element directly following the input */
+    $('[type=checkbox]').after('<span></span>');
+
     // replace HTML5 date inputs with JQuery UI if on a desktop as only Chrome and Opera support those
     if (!navigator.userAgent.match(/Mobi/)) {
         $('input[type=date]').each(function(idx) {
@@ -7,4 +12,5 @@ $(function() {
                    .datepicker({dateFormat: 'yy-mm-dd'});
         });
     }
+        
 });
